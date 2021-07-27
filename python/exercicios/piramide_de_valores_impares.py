fe90c9722b75a65f1eac1dfc6695e1c771621772
Daniel Cardoso <cardosodaniel75@gmail.com>
2021-07-27 19:19:08 -0300

    adiciona pasta exercicio com piramide_de_valores_impares.py

---
+++ python/exercicios/piramide_de_valores_impares.py
@@ -0,0 +1,74 @@
+"""
+loop entrada - 1
+    quant + 2
+    loop range()
+
+3 5 7 
+9 11 13 15 17
+
+atual + 2 * quant
+
+"""
+
+def somaDois(x):
+    return x+2
+
+#Obtem o primeiro numero da linha
+def obterNumeroInicial(numero_itens, numero_atual):
+    
+    #for y in range(0, numero_itens):
+    #    numero_atual = somaDois(numero_atual)
+    numero_atual = numero_atual + (numero_itens*2)
+
+    return numero_atual
+
+#Obtem o somatorio dos 3 ultimos numeros da linha
+def obtemSomatorio(numero_itens, numero_atual):
+    somatorio = 0
+
+    for y in range(0, numero_itens):
+        numero_atual = somaDois(numero_atual)
+        if(y+3 >= numero_itens):
+            somatorio += numero_atual
+        else:
+            continue
+
+    return somatorio
+
+def somatorioDesejado(numero_linhas_esperado):
+
+    numero_inicial = 1
+    numero_itens = 1
+
+    for i in range(1, int(numero_linhas_esperado)-1): 
+        numero_itens = somaDois(numero_itens)
+        numero_inicial = obterNumeroInicial(numero_itens, numero_inicial) 
+    
+    soma_tres_ultimos = obtemSomatorio(numero_itens+2, numero_inicial) 
+
+    return soma_tres_ultimos
+
+def main(numero_linhas_esperado):
+
+    if(type(numero_linhas_esperado) != int):
+        print("Nesse programa nao eh aceito \'"  + str(numero_linhas_esperado) + "\' como entrada!")
+
+    elif(numero_linhas_esperado <= 1 or numero_linhas_esperado > 10000000):
+        print("Numero de linhas "  + str(numero_linhas_esperado) + " diferente da aceita pelo programa!")
+        
+    else:
+        soma = somatorioDesejado(numero_linhas_esperado)
+        print(soma)
+        return soma
+
+if __name__ == "__main__":
+    
+    try:
+        main(1)
+        main(2)
+        main(3)
+        main(4)
+        main("a")
+        main(10000000)
+    except Exception as e:
+        print("Exception = " + str(e))
